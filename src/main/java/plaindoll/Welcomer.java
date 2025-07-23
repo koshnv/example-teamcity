5ac0d4b1b164921a118bc98a2f5c33b119ecf2b7
koshnv <zlatkvinteh@yandex.ru>
2025-07-23 19:16:24 +0000

    example-teamcity_add_reply_diff

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -13,4 +13,7 @@ public class Welcomer{
 	public String saySome(){
 		return "something in the way";
 	}
+    public String sayHunter() {
+            return "When the first hunter succumbed to the curse of blood, nightmare filled the streets of Kirova.";
+    }
 }
